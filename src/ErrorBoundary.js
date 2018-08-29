ca9fd41668c92e1a2f0d4e8caa32d167aed94ebf
Mehmet Ali Ayan <maliayan34@gmail.com>
2018-08-29 15:34:52 +0300

    Updated ErrorBoundary with an alert message

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -14,7 +14,7 @@ class ErrorBoundary extends React.Component {
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong, sorry!</h1>;
+      alert ( 'Something went wrong, sorry!' );
     }
     return this.props.children;
   }
