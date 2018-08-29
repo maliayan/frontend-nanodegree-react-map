bce6aff95f3ebe68078d8f7321f6d197320218cc
Mehmet Ali Ayan <maliayan34@gmail.com>
2018-08-29 10:23:23 +0300

    Added error handling for Foursquare API

--- src/FoursquareInfo.js
+++ src/FoursquareInfo.js
@@ -21,15 +21,20 @@ class FoursquareInfo extends Component {
      foursquare.venues.getVenuePhotos(params)
        .then(res=> {
          this.setState({ photos: res.response.photos.items });
-       });
+       })
+       .catch(error => {
+        alert(
+          "The photo cannot be loaded, sorry! Please check the clientID for Foursquare API."
+        );
+      });
    }
 
   render() {
     return (
     <div>
-        { this.state.photos.slice(0,1).map(photo => {
-          return <div key={photo.id}><img src={ photo.prefix + "300x300" + photo.suffix } alt={ "Photo of " + this.props.name } /><p>The photo is provided by Foursquare</p></div>
-        }) }
+      { this.state.photos.slice(0,1).map(photo => {
+        return <div key={photo.id}><img src={ photo.prefix + "300x300" + photo.suffix } alt={ "Photo of " + this.props.name } /><p>The photo is provided by Foursquare</p></div>
+      }) }
     </div>
   )
   }
